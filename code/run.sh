af85b02d67586bd042beb54054d95aa7b5201ad9
Wei Mao <50387842+wei-mao-2019@users.noreply.github.com>
2024-04-15 12:10:01 +1000

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-#ROOT=/home/wei/Documents/projects/2022-nerf/SDFFlow
-#SCAN=ian3
-##EXPNAME=_$SCAN
-##TIMESTAMP=2023_07_26_13_53_06_weights
-##CKPT=2000
-#GPU=0
-#
-### plot
-#CUDA_VISIBLE_DEVICES=$GPU python training/exp_runner.py --conf /home/wei/Documents/projects/2022-nerf/SDFFlow/code/confs/pretrain_flow.conf --gpu 0 --nepoch 2000 --is_pretrain_flow
+# used to initilize the sdf flow net if you want to train from scratch
+CUDA_VISIBLE_DEVICES=$GPU python training/exp_runner.py --conf /home/wei/Documents/projects/2022-nerf/SDFFlow/code/confs/pretrain_flow.conf --gpu 0 --nepoch 2000 --is_pretrain_flow
 
 
+# training script
 ROOT=/home/wei/Documents/projects/2022-nerf/SDFFlow
 GPU=0
 
-#python training/exp_runner.py --conf ./confs/total_capture.conf --gpu 0 --nepoch 2000 \
-#     --is_dist --gpus 2 --cancel_vis --is_only_plot
+python training/exp_runner.py --conf ./confs/total_capture.conf --gpu 0 --nepoch 2000 \
+     --is_dist --gpus 2 --cancel_vis --is_only_plot
 
+# evaluation script
 SCAN=ian3
 EXPNAME=_$SCAN
 TIMESTAMP=2023_11_29_17_10_04
 CKPT=2000
 
-## plot
-#python training/exp_runner.py --conf $ROOT/exps/total_capture$EXPNAME/$TIMESTAMP/runconf.conf --gpu 0 --nepoch 2000 \
-#       --cancel_vis --is_only_plot --is_continue --checkpoint $CKPT
+# plot results
+python training/exp_runner.py --conf $ROOT/exps/total_capture$EXPNAME/$TIMESTAMP/runconf.conf --gpu 0 --nepoch 2000 \
+       --cancel_vis --is_only_plot --is_continue --checkpoint $CKPT
 
+# evaluate the geometry
 python evaluation/eval_flow_geo.py --conf $ROOT/exps/total_capture$EXPNAME/$TIMESTAMP/runconf.conf \
        --checkpoint $CKPT --timestamp $TIMESTAMP
 
